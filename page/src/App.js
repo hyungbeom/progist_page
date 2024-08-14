a79c04783f7252937e798d769dd1dd0dcd13d554
beomlee <lhbhb21c@gmail.com>
2024-08-14 16:42:55 +0900

    [create] o conetents card sample 작성 - hblee

--- page/src/App.js
+++ page/src/App.js
@@ -3,11 +3,20 @@ import './resource/css/body.css';
 import LandingPage from "./components/home/LandingPage";
 import IntroPageOut from "./components/home/IntroPageOut";
 import Header from "./components/temp/Header";
+import InfoCard from "./components/temp/InfoCard";
+
 function App() {
     return (
         <div>
 
             <Header/>
+
+            <InfoCard
+                width={200}
+                img={'123'} title={'…not reliable due to a lack of resilience'}
+                      subTitle={'One single incident such as a ship getting stuck in the Suez Canal or a labor strike in a port shuts down major parts of global shipping, leads to massive congestion and ultimately causes damages in the billions in just a few days.'}
+            />
+
         </div>
     );
 }
---
+++ page/src/components/temp/InfoCard.js
@@ -0,0 +1,13 @@
+import {useEffect, useState} from "react";
+
+export default function InfoCard({img ='', title ='', subTitle ='', width = 100}){
+
+
+
+
+    console.log(img,'info:')
+
+    return (
+        <div style={{backgroundColor : "red", width : width}}>asdfasdf</div>
+    )
+}
