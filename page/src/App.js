9882c4682d559fafd71eb1df4cd44d0a5e25e17e
12gina <cometome1212@gmail.com>
2024-08-14 16:04:31 +0900

    a

--- page/src/App.js
+++ page/src/App.js
@@ -1,9 +1,13 @@
 import IntroPage from "./components/home/IntroPage";
 import './resource/css/body.css';
+import IntroPageOut from "./components/home/IntroPageOut";
+import Header from "./components/temp/Header";
 function App() {
     return (
         <div>
-            <IntroPage/>
+            {/*<IntroPage/>*/}
+            {/*<IntroPageOut/>*/}
+            <Header/>
         </div>
     );
 }
