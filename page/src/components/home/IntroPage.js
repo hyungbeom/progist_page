6590051e3c5b173fd5bb6c1315010da72ec82304
beomlee <lhbhb21c@gmail.com>
2024-08-14 11:41:44 +0900

    [create] o react 초기세팅 및 랜딩페이지 sample 작성 - hblee

--- page/src/components/home/IntroPage.js
+++ page/src/components/home/IntroPage.js
@@ -2,7 +2,7 @@ export default function IntroPage() {
 
 
     return <div style={{backgroundColor: '#C17F00', height: '100vh'}}>
-        <div style={{padding: '263px 0px 0px 70px', color: "white"}}>
+        <div style={{padding: '263px 70px 0px 70px', color: "white"}}>
             <div style={{opacity: 0.7}}><span style={{fontSize: 228}}> 80</span> <span style={{fontSize: 100}}>%</span>
             </div>
             <div style={{height: 50, borderBottom: '3px solid white'}}/>
