032db1bacfc775f7204220befb676848ec8fe3f2
beomlee <lhbhb21c@gmail.com>
2024-08-16 15:49:27 +0900

    [create] o Footer.js 샘플 컴포넌트 자료 작성 - hblee

--- page/src/App.js
+++ page/src/App.js
@@ -1,11 +1,13 @@
 import './resource/css/body.css';
 import IntroPage from "./components/home/IntroPage";
 import Header from "./components/temp/Header";
+import Footer from "./components/temp/Footer";
 
 function App() {
     return (<>
-            <Header/>
-            <IntroPage/>
+            {/*<Header/>*/}
+            {/*<IntroPage/>*/}
+            <Footer/>
         </>
     );
 }
