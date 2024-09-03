077c6cd24ae128ea5e2c1eb1087a43e99818fcda
Gina <cometome1212@gmail.com>
2024-09-03 18:34:58 +0900

    [update] home&header

--- page/src/page/Contact.js
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-    return (
-        <>
-            Contact
-        </>
-    );
-};
-
-export default Contact;
--- page/src/resource/contents/MenuList.js
+++
@@ -1,7 +0,0 @@
-export const menuList=[
-        {title: 'Home.', link: '/'},
-        {title: 'About Us.', link: '/aboutus'},
-        {title: 'progress.', link: '/progress'},
-        {title: 'Recruit.', link: '/recruit'},
-        {title: 'Contact.', link: '/contact'},
-    ]
